bbacce7e79f75f1878b3ffa1a1c465315f9c4040
Chris Lucas <chris.lucas@bristol.ac.uk>
2014-12-08 14:51:13 +0000

    update trigger effs; add sitv plot mechanisms

--- DiMuon_Plot_Producer.py
+++ DiMuon_Plot_Producer.py
@@ -12,45 +12,36 @@ import shutil
 from run_details import this_run
 
 
-trigger_effs =  { "150_Low":0.970,"150_High":0.972,
-                  "200_Low":0.970,"200_High":0.972,
-                  "275_Low":0.972,"275_High":0.973,
-                  "325_Low":0.973,"325_High":0.973,
-                  "375_Low":0.974,"375_High":0.972,
-                  "475_Low":0.976,"475_High":0.973,
-                  "575_Low":0.977,"575_High":0.974,
-                  "675_Low":0.979,"675_High":0.977,
-                  "775_Low":0.977,"775_High":0.973,
-                  "875_Low":0.978,"875_High":0.978,
-                  "975_Low":0.980,"975_High":0.978,
-                  "1075_Low":0.977,"1075_High":0.979,}
-
-
-# For 30/30 dimu selection
-trigger_effs =  { "150_Low":0.989,"150_High":0.988,
-                  "200_Low":0.989,"200_High":0.988,
-                  "275_Low":0.989,"275_High":0.988,
-                  "325_Low":0.989,"325_High":0.989,
-                  "375_Low":0.989,"375_High":0.989,
-                  "475_Low":0.989,"475_High":0.989,
-                  "575_Low":0.989,"575_High":0.989,
-                  "675_Low":0.990,"675_High":0.989,
-                  "775_Low":0.989,"775_High":0.989,
-                  "875_Low":0.990,"875_High":0.989,
-                  "975_Low":0.990,"975_High":0.991,
-                  "1075_Low":0.990,"1075_High":0.990}
+trigger_effs =  {"150_Low": 0.984,"150_High": 0.984,
+                  "200_Low": 0.985,"200_High": 0.984,
+                  "275_Low": 0.985,"275_High": 0.984,
+                  "325_Low": 0.986,"325_High": 0.986,
+                  "375_Low": 0.986,"375_High": 0.985,
+                  "475_Low": 0.986,"475_High": 0.986,
+                  "575_Low": 0.986,"575_High": 0.986,
+                  "675_Low": 0.987,"675_High": 0.986,
+                  "775_Low": 0.986,"775_High": 0.986,
+                  "875_Low": 0.987,"875_High": 0.986,
+                  "975_Low": 0.987,"975_High": 0.988,
+                  "1075_Low":0.987,"1075_High":0.987,}
 
 settings = {
-  "dirs":["150_200","200_275","275_325","325_375","375_475","475_575","575_675","675_775","775_875","875_975","975_1075","1075"],
-  "Plots":["MHTovMET_all","HT_all","DiMuon_Mass_all", "MHT_all","AlphaT_all","JetMultiplicity_all","Number_Btags_all","CommonJetPt_all","CommonJetEta_all","MuPt_all","MuEta_all","SecondMuPt_all","SecondMuEta_all","MuPFIso_all","Number_Good_verticies_all"],
+  "dirs":["150_200","200_275","275_325","325_375","375_475","475_575","575_675","675_775","775_875","875_975","975_1075","1075"][4:],
+  # "Plots":["MHTovMET_all","MET_Corrected_all", "MET_all", "MHT_all","HT_all","DiMuon_Mass_all", "AlphaT_all","JetMultiplicity_all","Number_Btags_all","CommonJetPt_all","CommonJetEta_all","MuPt_all","MuEta_all","SecondMuPt_all","SecondMuEta_all","MuPFIso_all","Number_Good_verticies_all","LeadJetPt_all","SecondJetPt_all"][1:2],
+  "Plots":["ComMinBiasDPhi_all", "ComMinBiasDPhi_acceptedJets_all"],
   "Lumo" : this_run()["mu_lumi"]*10.,
   "Webpage":"btag",
   "Category":"DiMuon",
-  "WebBinning":["150_200","200_275","275_325","325_375","200_upwards","375_upwards"],
+  "WebBinning":["150_200","200_275","275_325","325_375","200_upwards","375_upwards"][-1:],
   "Misc":[],
-  "MHTMET":"True",
-  "Trigger":trigger_effs
+  "MHTMET":"False",
+  "Trigger":trigger_effs,
+  "SITV_plots":[False, True][0]
   }
+
+if settings["SITV_plots"]:
+  for p in ['pfCandsPt_all', 'pfCandsDzPV_all', 'pfCandsDunno_all', 'pfCandsCharge_all']:
+    settings["Plots"].append(p)
 
 print ">> Opening directory:", this_run()["path_name"]
 sleep(3)
@@ -78,6 +69,7 @@ muon_plots = {
      "mc7":("./"+rootpath+"/Muon_DiBoson.root","DiMuon_","Di-Boson","Muon","Inclusive"),
      # "mc8":("./"+rootpath+"/Muon_QCD.root","DiMuon_","QCD","Muon","Inclusive"), 
      "mc9":("./"+rootpath+"/Muon_SingleTop.root","DiMuon_","Single_Top","Muon","Inclusive"),
+     # "mc10":("./"+rootpath+"/Muon_SMS.root","DMuon_","SMS","Muon","Inclusive"),
     }
 
 muon_one_btag_plots = {
@@ -88,6 +80,7 @@ muon_one_btag_plots = {
      "mcb5":("./"+rootpath+"/Muon_DY"+njet_ext+".root","btag_one_DiMuon_","DY","Muon","One"),
      "mcb6":("./"+rootpath+"/Muon_SingleTop.root","btag_one_DiMuon_","Single_Top","Muon","One"),
      "mcb7":("./"+rootpath+"/Muon_DiBoson.root","btag_one_DiMuon_","Di-Boson","Muon","One"),
+     # "mcb9":("./"+rootpath+"/Muon_SMS.root","btag_one_DiMuon_","SMS","Muon","One"),
     }
 
 muon_two_btag_plots = {
@@ -99,6 +92,7 @@ muon_two_btag_plots = {
      "mcb5":("./"+rootpath+"/Muon_DY"+njet_ext+".root","btag_two_DiMuon_","DY","Muon","Two"),
      "mcb6":("./"+rootpath+"/Muon_SingleTop.root","btag_two_DiMuon_","Single_Top","Muon","Two"),
      "mcb7":("./"+rootpath+"/Muon_DiBoson.root","btag_two_DiMuon_","Di-Boson","Muon","Two"), 
+     # "mcb9":("./"+rootpath+"/Muon_SMS.root","btag_two_DiMuon_","SMS","Muon","Two"),
     }
 
 muon_zero_btag_plots = {
@@ -108,7 +102,8 @@ muon_zero_btag_plots = {
      "mcb4":("./"+rootpath+"/Muon_Zinv.root","btag_zero_DiMuon_","Zinv","Muon","Zero"),
      "mcb5":("./"+rootpath+"/Muon_DY"+njet_ext+".root","btag_zero_DiMuon_","DY","Muon","Zero"),
      "mcb6":("./"+rootpath+"/Muon_SingleTop.root","btag_zero_DiMuon_","Single_Top","Muon","Zero"),
-     "mcb7":("./"+rootpath+"/Muon_DiBoson.root","btag_zero_DiMuon_","Di-Boson","Muon","Zero"),  
+     "mcb7":("./"+rootpath+"/Muon_DiBoson.root","btag_zero_DiMuon_","Di-Boson","Muon","Zero"),
+     # "mcb9":("./"+rootpath+"/Muon_SMS.root","btag_zero_DiMuon_","SMS","Muon","Zero"),
     }
 
 muon_morethanzero_btag_plots = {
@@ -119,21 +114,17 @@ muon_morethanzero_btag_plots = {
      "mcb5":("./"+rootpath+"/Muon_DY"+njet_ext+".root","btag_morethanzero_DiMuon_","DY","Muon","Zero"),
      "mcb7":("./"+rootpath+"/Muon_DiBoson.root","btag_morethanzero_DiMuon_","Di-Boson","Muon","Zero"),
      "mcb9":("./"+rootpath+"/Muon_SingleTop.root","btag_morethanzero_DiMuon_","Single_Top","Muon","Zero"),
+     # "mcb9":("./"+rootpath+"/Muon_SMS.root","btag_morethanzero_DiMuon_","SMS","Muon","Zero"),
     }
 
 if __name__=="__main__":
   a = Plotter(settings,muon_plots,jet_multiplicity = "True",make_ratio= "True")
-  b = Plotter(settings,muon_morethanzero_btag_plots,jet_multiplicity = "True",make_ratio= "True")
-  c = Plotter(settings,muon_two_btag_plots,jet_multiplicity = "True",make_ratio= "True")
+  # b = Plotter(settings,muon_morethanzero_btag_plots,jet_multiplicity = "True",make_ratio= "True")
+  # c = Plotter(settings,muon_two_btag_plots,jet_multiplicity = "True",make_ratio= "True")
   d = Plotter(settings,muon_zero_btag_plots,jet_multiplicity = "True",make_ratio= "True")
   e = Plotter(settings,muon_one_btag_plots,jet_multiplicity = "True",make_ratio= "True")
-  """
-  settings["Misc"] = ["NoLegend"]
-  settings["Lumo"] = 1.0
-  settings["Plots"] = ["MHT_vs_MET_all","MET_vs_MHTovMET_all"]
-  #Plotter(settings,dimuon_2d,jet_multiplicity = "True",draw_data="False")
-  settings["Plots"] = ["MET_vs_MHTovMET_all","MHT_vs_MET_all","MHTovMET_all","MHTovMET_Scaled_all","MET_all","MET_Corrected_all","MHT_all","MHT_FixedThreshold_all","JetMultiplicity_all","HT_all", "JetPt_all","JetEta_all"]
-  """
+
   finish = Webpage_Maker(settings["Plots"],settings["WebBinning"],settings["Category"],option=settings["Webpage"])
+
   try :shutil.rmtree('./Plots')
   except OSError as exc: pass
